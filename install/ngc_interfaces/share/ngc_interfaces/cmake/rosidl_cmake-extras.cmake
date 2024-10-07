fb6b36c2307a0f5d9badcded5bad8ad91c12ba7a
sonhar90 <sondreharam@gmail.com>
2024-10-07 11:37:56 +0200

    Slettet all kode fra main-grenen

--- install/ngc_interfaces/share/ngc_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(ngc_interfaces_IDL_FILES "msg/Eta.idl;msg/Nu.idl;msg/NuDot.idl;msg/Tau.idl;msg/Wind.idl;msg/ThrusterSignals.idl;msg/GNSS.idl;msg/HeadingDevice.idl")
-set(ngc_interfaces_INTERFACE_FILES "msg/Eta.msg;msg/Nu.msg;msg/NuDot.msg;msg/Tau.msg;msg/Wind.msg;msg/ThrusterSignals.msg;msg/GNSS.msg;msg/HeadingDevice.msg")
